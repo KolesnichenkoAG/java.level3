ef261a533abfecbfdca16d328bd3cda828b09bf1
KolesnichenkoAG <kag.1981@mail.ru>
2022-08-27 23:41:08 +0600

    дз окончено1

--- src/main/java/lesson1homework/Main.java
+++ src/main/java/lesson1homework/Main.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        Integer[] intArr = {1, 2, 3, 4, 5};
+        Integer[] intArr = {1, 2, 3, 4, 5, 6, 7};
         Double[] doubleArr = {1.1, 1.2, 1.3, 1.4};
 
         System.out.println(Arrays.toString(intArr));
